bb8a6b0e8ebd896d439a6c86a8884dc35a53db49
ManuKai <manumagnificent402@gmail.com>
2024-07-03 10:46:43 +0300

    more changes to build.sh

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,7 +1,13 @@
-set -o errexit
+#!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
