adbc59980d9f139e03daa3d79f7bdfe0d317ef66
ManuKai <manumagnificent402@gmail.com>
2024-08-05 18:14:08 +0300

    comment

--- Backend/styleSavvyBackend/settings.py
+++ Backend/styleSavvyBackend/settings.py
@@ -123,7 +123,7 @@ DATABASES = {
 #     'NAME': BASE_DIR / 'db.sqlite3',
 # }
 # }    
-
+# comment
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
